0d7d61e02d296ce842a2b824a43706201a94b1f7
zhonghuawu <wu_zhonghua@outlook.com>
2017-03-07 17:20:32 +0800

    add run_all function in ColonTumor.py

--- ColonTumor.py
+++ ColonTumor.py
@@ -5,7 +5,6 @@ from sklearn import datasets
 from sklearn import cross_validation
 from sklearn.cross_validation import train_test_split
 
-
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import roc_auc_score
@@ -26,8 +25,26 @@ def run(X, y):
 
     cm = confusion_matrix(y_test, y_pred)
     print cm
+    print "y_test = ", y_test
+    print "y_pred = ", y_pred
     print "MCC: %f"%matthews_corrcoef(y_test, y_pred)
     print "ACC: %f"%accuracy_score(y_test, y_pred)
+
+def run_all(X, y):
+    print X.shape, y.shape
+    clf = svm.SVC()
+    #clf = svm.LinearSVC()
+    scores = cross_validation.cross_val_score(clf, X, y, cv=5, scoring="accuracy")
+    print "scores: ", scores, scores.mean()
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+
+    cm = confusion_matrix(y, y_pred)
+    print cm
+    print "y_test = ", y
+    print "y_pred = ", y_pred
+    print "MCC: %f"%matthews_corrcoef(y, y_pred)
+    print "ACC: %f"%accuracy_score(y, y_pred)
 
 if __name__ == "__main__":
     fr_n = r"/home/huaa/workspace/datas/gene_expr/ColonTumor/colonTumor.data"
@@ -38,9 +55,13 @@ if __name__ == "__main__":
     y = [0 if i==yy[0] else 1 for i in yy]
     X, y = numpy.array(X), numpy.array(y)
     print "origion datas:"
-    run(X, y)
-    index = [2, 8, 10, 13, 14, 22, 25, 42, 48, 61, 65, 71, 74, 110, 137, 186, 244, 248, 265, 364, 376, 390, 426, 492, 526, 600, 624, 697, 764, 801, 978, 1023, 1046, 1059, 1152, 1324, 1345, 1369, 1422, 1465, 1472, 1581, 1607, 1770, 1771, 1774, 1869]
+    #run(X, y)
+    run_all(X, y)
+    #index = [2, 8, 10, 13, 14, 22, 25, 42, 48, 61, 65, 71, 74, 110, 137, 186, 244, 248, 265, 364, 376, 390, 426, 492, 526, 600, 624, 697, 764, 801, 978, 1023, 1046, 1059, 1152, 1324, 1345, 1369, 1422, 1465, 1472, 1581, 1607, 1770, 1771, 1774, 1869]
+    #index = [1974, 1993]
+    index = [14, 193]
     X = X[:, index]
     print "after features selection:"
-    run(X, y)
+    #run(X, y)
+    run_all(X, y)
     print "Done!"
