5add3cc781db4980d50270e59d2d43496126e99b
zhonghua_wu <wu_zhonghua@outlook.com>
2017-08-15 16:25:25 +0800

    change aggr.png 2*3 to 3*2 and save to aggr_vertical.png

--- datas/gene/all_result/streaming/aggr_png_6.py
+++ datas/gene/all_result/streaming/aggr_png_6.py
@@ -2,17 +2,19 @@ from matplotlib import image
 import numpy as np
 
 if __name__=='__main__':
-    img_names="TOX_171 SMK_CAN_187 ALLAML".split()
-    img1=image.imread("%s.png"%img_names[0])
-    img1=np.concatenate((img1, image.imread("%s.png"%img_names[1])), axis=1)
-    img1=np.concatenate((img1, image.imread("%s.png"%img_names[2])), axis=1)
-    
-    img_names="GLI_85 Prostate_GE Lung_Cancer".split()
-    img2=image.imread("%s.png"%img_names[0])
-    img2=np.concatenate((img2, image.imread("%s.png"%img_names[1])), axis=1)
-    img2=np.concatenate((img2, image.imread("%s.png"%img_names[2])), axis=1)
+    img_names="TOX_171 SMK_CAN_187".split()
+    img1=np.concatenate((image.imread("%s.png"%img_names[0]),image.imread("%s.png"%img_names[1])), axis=1)
+    # img1=image.imread("%s.png"%img_names[0])
+    # img1=np.concatenate((img1, image.imread("%s.png"%img_names[1])), axis=1)
+    # img1=np.concatenate((img1, image.imread("%s.png"%img_names[2])), axis=1)
 
-    img=np.concatenate((img1, img2))
-    image.imsave("aggr.png", img)
+    img_names="ALLAML GLI_85".split()
+    img2=np.concatenate((image.imread("%s.png"%img_names[0]),image.imread("%s.png"%img_names[1])), axis=1)
+    
+    img_names="GLI_85 Prostate_GE".split()
+    img3=np.concatenate((image.imread("%s.png"%img_names[0]),image.imread("%s.png"%img_names[1])), axis=1)
+
+    img=np.concatenate((img1, img2, img3))
+    image.imsave("aggr_vertical.png", img)
     
 
