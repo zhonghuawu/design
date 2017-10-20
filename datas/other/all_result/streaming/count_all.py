1db5aef7cfbf5a7832695fb9db31cb4e1b041b17
Huaa <wu_zhonghua@outlook.com>
2017-10-20 11:10:50 +0800

    draw dataset.png that dataset are in datas/data_conf.txt

---
+++ datas/other/all_result/streaming/count_all.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+
+def get_datasets_name():
+    datasets_name = []
+    with open('all_attribute.csv', 'r') as f:
+        f.readline()
+        for line in f:
+            datasets_name.append(line.split(',')[0])
+    return datasets_name
+
+def read_output_streaming_cls_nfs_one_dataset(alg):
+    res_cls = pd.Series(())
+    res_nfs = pd.Series(())
+    datasets_name = get_datasets_name()
+    for dataset_name in datasets_name:
+        with open("%s/%s_cls.output_%s"%(alg, dataset_name, alg), 'r') as f:
+            for line in f:
+                line_list = line.split()
+                if line_list[0]=='size' and line_list[2] == 'data':
+                    res_nfs[dataset_name] = int(line_list[-1][:-1])
+                if line_list[0]=='cross':
+                    res_cls[dataset_name] = float(line_list[-1])
+    res_cls.name = alg
+    res_nfs.name = alg
+    return res_cls, res_nfs
+    
+
+def main():
+    # datasets_name = get_datasets_name()
+    alg = 'streaming_grafting'
+    cls_grafting, nfs_grafting = read_output_streaming_cls_nfs_one_dataset(alg)
+
+    alg = 'streaming_l21'
+    cls_l21, nfs_l21 = read_output_streaming_cls_nfs_one_dataset(alg)
+    cls = pd.concat((cls_l21, cls_grafting), axis=1)
+    nfs = pd.concat((nfs_l21, nfs_grafting), axis=1)
+    print cls
+    print nfs
+
+if __name__ == '__main__':
+    main()
---
+++ datas/text/all_result/steaming/draw.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_output_streaming_cls_nfs(fname):
+    res_cls = pd.Series(())
+    res_nfs = pd.Series(())
+    with open(fname, 'r') as f:
+        epsilon = 0.0
+        for line in f:
+            line_list = line.strip().split()
+            if line_list[0] == 'after':
+                epsilon = float(line_list[-1][:-1])
+            if line_list[0] == 'cross':
+                res_cls[epsilon] = float(line_list[-1])
+            if line_list[0] == 'size' and line_list[2] == 'data':
+                res_nfs[epsilon] = int(line_list[-1][:-1])
+    res_cls.name = 'prediction accuracy'
+    res_nfs.name = 'number of selected features'
+    return res_cls.drop(0.0), res_nfs.drop(0.0)
+
+def read_output_streaming(fname):
+    cls = pd.Series(())
+    with open(fname, 'r') as f:
+        percentage = "1000"
+        for line in f:
+            line_list = line.strip().split()
+            if line_list[0] == 'after':
+                percentage = line_list[3][:-1]
+            if line_list[0] == 'cross':
+                cls[percentage] = float(line_list[-1])
+    # cls.name = 'prediction accuracy'
+    return cls.drop('1000')
+
+def read_output_streaming_of_one_dataset(dataset_name):
+    alg = "sfs_l21"
+    cls_l21 = read_output_streaming("../streaming_l21/%s_cls.output_streaming"%dataset_name)
+    cls_l21.name = alg
+    cls = cls_l21
+
+    algs = "grafting osfs Alpha_investing saola".split()
+    for alg in algs:
+        fname = "../streaming_%s/%s_cls.output_streaming"%(alg, dataset_name)
+        cls_tmp = read_output_streaming(fname)
+        cls_tmp.name = alg
+        cls = pd.concat((cls, cls_tmp), axis=1)
+
+#     alg = "grafting"
+#     cls_grafting = read_output_streaming("../streaming_%s/%s_cls.output_streaming_%s"%(alg, dataset_name, alg))
+#     cls_grafting.name = alg
+
+#     alg = "osfs"
+#     cls_osfs = read_output_streaming("../streaming_%s/%s_cls.output_streaming_%s"%(alg, dataset_name, alg))
+#     cls_osfs.name = alg
+
+#     alg = "Alpha_investing"
+#     cls_Alpha_investing = read_output_streaming("../streaming_%s/%s_cls.output_streaming_%s"%(alg, dataset_name, alg))
+#     cls_Alpha_investing.name = alg
+
+    # cls = pd.concat((cls_l21, cls_grafting, cls_osfs, cls_Alpha_investing), axis=1)
+    return cls
+    
+def draw(cls, fname):
+    fig, ax = plt.subplots(1, 1)
+    style='o- ^-- s-. p: D:'.split(' ')
+    cls.plot(ax=ax, style=style, ylim=(0.4, 1.0))
+    #nfs.plot(ax=axes[1], style='*-', ylim=(0, 90))
+    ax.set_ylabel('Prediction accuracy')
+    ax.set_xlabel('The percentage of features streaming in (%)')
+    ax.set_title(fname)
+
+    # fig.set_size_inches(12, 8)
+    fig.set_size_inches(6, 4)
+    fig.savefig('images/%s.png'%fname, bbox_inches='tight')
+    plt.close()
+
+def get_datasets_name():
+    datasets_name = []
+    with open('../all_attribute.csv', 'r') as f:
+        f.readline()
+        for line in f:
+            if line.startswith('#'):
+                continue
+            datasets_name.append(line.split(',')[0])
+    return datasets_name
+
+def main():
+    datasets_name = get_datasets_name()
+    for dataset_name in datasets_name:
+        print "draw %s"%dataset_name
+        cls = read_output_streaming_of_one_dataset(dataset_name)
+        cls.to_csv("csvs/%s.csv"%dataset_name)
+        draw(cls, dataset_name)
+        print "finish %s"%dataset_name
+
+
+if __name__ == '__main__':
+    main()
+    print "Done!"
+    
