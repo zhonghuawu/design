a2dd22853ac229a84f15a56e97ad11a0dcff39ce
zhonghuawu <wu_zhonghua@outlook.com>
2017-10-23 11:14:57 +0800

    repaint all datasets image that all datasets are in datas\data_conf.txt

--- datas/text/all_result/count_all.py
+++ datas/text/all_result/count_all.py
@@ -6,6 +6,8 @@ def get_datasets_name():
     with open('all_attribute.csv', 'r') as f:
         f.readline()
         for line in f:
+            if line.startswith('#'):
+                continue
             datasets_name.append(line.split(',')[0])
     return datasets_name
 
@@ -14,11 +16,11 @@ def read_output_streaming_cls_nfs_one_dataset(alg):
     res_nfs = pd.Series(())
     datasets_name = get_datasets_name()
     for dataset_name in datasets_name:
-        with open("%s/%s_cls.output_%s"%(alg, dataset_name, alg), 'r') as f:
+        with open("streaming_%s/%s_cls.output_streaming"%(alg, dataset_name), 'r') as f:
             for line in f:
                 line_list = line.split()
                 if line_list[0]=='size' and line_list[2] == 'data':
-                    res_nfs[dataset_name] = int(line_list[-1][:-1])
+                    res_nfs[dataset_name] = long(line_list[-1][:-1])
                 if line_list[0]=='cross':
                     res_cls[dataset_name] = float(line_list[-1])
     res_cls.name = alg
@@ -27,15 +29,16 @@ def read_output_streaming_cls_nfs_one_dataset(alg):
     
 
 def main():
-    # datasets_name = get_datasets_name()
-    alg = 'streaming_grafting'
-    cls_grafting, nfs_grafting = read_output_streaming_cls_nfs_one_dataset(alg)
+    alg = 'l21'
+    cls, nfs= read_output_streaming_cls_nfs_one_dataset(alg)
 
-    alg = 'streaming_l21'
-    cls_l21, nfs_l21 = read_output_streaming_cls_nfs_one_dataset(alg)
-
-    cls = pd.concat((cls_l21, cls_grafting), axis = 1)
-    nfs = pd.concat((nfs_l21, nfs_grafting), axis = 1)
+    algs = 'grafting osfs Alpha_investing saola'
+    for alg in algs.split():
+        cls_tmp, nfs_tmp = read_output_streaming_cls_nfs_one_dataset(alg)
+        cls = pd.concat((cls, cls_tmp), axis=1)
+        nfs = pd.concat((nfs, nfs_tmp), axis=1)
+    cls.to_csv("all_cls.csv")
+    nfs.to_csv("all_nfs.csv")
     print cls
     print nfs
 
