72c742b5174f956f99b5810b10bf5f64e2d9b8d8
zhonghuawu <wu_zhonghua@outlook.com>
2017-12-10 14:54:02 +0800

    draw aggr_one.png of epsilon and lambda

--- datas/gene/all_result/threshold/aggr_png_4_threshold.py
+++ datas/gene/all_result/threshold/aggr_png_4_threshold.py
@@ -4,14 +4,14 @@ import numpy as np
 if __name__=='__main__':
     # img_names="SMK_CAN_187 GLI_85 Prostate_GE DLBCL".split()
     img_names="SMK_CAN_187 GLIOMA Prostate_GE SRBCT".split()
-    img1=image.imread("%s.png"%img_names[0])
-    img1=np.concatenate((img1, image.imread("%s.png"%img_names[1])), axis=1)
+    img1=image.imread("%s_one.png"%img_names[0])
+    img1=np.concatenate((img1, image.imread("%s_one.png"%img_names[1])), axis=1)
 
-    img2=image.imread("%s.png"%img_names[2])
-    img2=np.concatenate((img2, image.imread("%s.png"%img_names[3])), axis=1)
+    img2=image.imread("%s_one.png"%img_names[2])
+    img2=np.concatenate((img2, image.imread("%s_one.png"%img_names[3])), axis=1)
 
     img=np.concatenate((img1, img2))
 
-    image.imsave("aggr.png", img)
+    image.imsave("aggr_one.png", img)
 
 
